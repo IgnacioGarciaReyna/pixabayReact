fa7c8cc718f46b64e08385e5d244b70e737477d9
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-08-16 19:29:15 -0300

    Se agregó un Error y la consulta a la API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Formulario from "./components/Formulario";
 
 function App() {
+  //State de la app
+  const [busqueda, guardarBusqueda] = useState("");
+
+  //Realizar la búsqueda
+  useEffect(() => {
+    //Validación para saber si es la primer carga, ya que no queremos que haga ninguna consulta
+    if (busqueda === "") {
+      return;
+    }
+
+    const consultarAPI = async () => {
+      const imagenesPorPagina = 30;
+      const key = "29321465-d95ff742352fd9f9167485be6";
+
+      //"per_page" es un parametro que está en la documentación, sirve para paginación
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarBusqueda(resultado.hits);
+    };
+    consultarAPI();
+  }, [busqueda]);
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center"></p>
 
-        <Formulario />
+        <Formulario guardarBusqueda={guardarBusqueda} />
       </div>
     </div>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Error from "./Error";
 
-const Formulario = () => {
+const Formulario = ({ guardarBusqueda }) => {
+  //State para guardar lo que escribe el usuario en el buscador
+  const [termino, guardarTermino] = useState("");
+  //State para el error
+  const [error, guardarError] = useState(false);
+
+  //Función que se ejecuta en el submit
+  const buscarImagenes = (e) => {
+    e.preventDefault();
+
+    //Validar
+    if (termino.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    //Esta parte funciona como un else, porque si cumple la condición no sigue ejecutando, hace un return
+    guardarError(false);
+
+    //Enviar el termino de búsqueda hacia el componente principal
+    guardarBusqueda(termino);
+  };
+
   return (
-    <form>
+    <form onSubmit={buscarImagenes}>
       <div className="row">
         <div className="form-group col-md-8">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Busca una imagen, por ejemplo: futbol o cafe"
+            onChange={(e) => guardarTermino(e.target.value)}
           />
         </div>
         <div className="form-group col-md-4">
@@ -19,6 +42,9 @@ const Formulario = () => {
           />
         </div>
       </div>
+      {error ? (
+        <Error mensaje="Por favor, agrega un termino de búsqueda" />
+      ) : null}
     </form>
   );
 };
